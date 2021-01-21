51f09de512fcd181585457a3b83c2b76499a0ce1
Jonathan  Tran <110579@eesd.org>
2021-01-21 04:18:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,58 @@
+'''
+01/14/2021
+
+C1: input
+
+Review: Output
+>> print ("hello world")
+OUTPUT: hello world
+
+Input 
+The way the computer gets information from the User
+
+Python initially treats inputs as a string type
+
+Ex.
+5 --> "5"
+hi --> "hi"
+
+Formula creating a variable:
+  varibleName = Data/Value
+
+Formula Creating an input variable
+variableName = input(PROMPT)
+
+'''
+
+Name = "Jonathan"#string
+
+print ("My name is " + Name)
+
+name1 = input("What is your name: ")
+
+print ("My name is " + name1)
+
+#Ask user for school
+#Ask user for grade 
+#Ask user for favorite color
+#Ask user for the years they were born
+#Ask user for age
+
+school = input("Enter your school: ")
+
+grade = input("Enter your grade: ")
+
+favoriteColor = input("Enter your favorite color: ")
+
+year = input("Enter the year that you were born: ")
+
+age = input("Enter your age: ")
+
+print ("You go to " + school)
+print ("You're in " + grade + " grade")
+print ("Your favorite color is " + favoriteColor)
+print ("The year that I was born is " + year)
+print ("I am " + age + " years old")
+
+print ("")
+
